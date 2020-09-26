9c300a200b1822a85980d14b5edadd0fc7aea5d8
st0ro <46041969+st0ro@users.noreply.github.com>
2020-09-25 19:51:38 -0600

    GUI code upload

--- src/borderhacks2020/Main.java
+++ src/borderhacks2020/Main.java
@@ -1,16 +1,31 @@
 package borderhacks2020;
 
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Main extends StateBasedGame {
-    public Main(String name) {
-        super(name);
+
+    public static final int WIDTH = 1000, HEIGHT = 600;
+
+    public Main() {
+        super("Pandemic!!!");
     }
 
     @Override
     public void initStatesList(GameContainer gameContainer) throws SlickException {
 
+    }
+
+    public static void main(String args[]){
+        try {
+            AppGameContainer app = new AppGameContainer(new Main()); //create game in container
+            app.setDisplayMode(WIDTH, HEIGHT, false); //set window size and if fullscreen
+            app.setVSync(true);
+            app.start();
+        } catch (SlickException e) {
+            e.printStackTrace(); //in case of failure to start game, will print error
+        }
     }
 }
---
+++ src/borderhacks2020/mainmenu/MainMenu.java
@@ -0,0 +1,21 @@
+package borderhacks2020.mainmenu;
+
+import borderhacks2020.EventBasedState;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class MainMenu extends EventBasedState {
+
+    @Override
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public int getID() {
+        return 0;
+    }
+}
