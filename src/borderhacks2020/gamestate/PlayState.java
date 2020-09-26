f46643069b0ef46e19016897ebf4befdf94c5342
st0ro <46041969+st0ro@users.noreply.github.com>
2020-09-26 16:30:24 -0600

    SQUARE

--- src/borderhacks2020/gamestate/PlayState.java
+++ src/borderhacks2020/gamestate/PlayState.java
@@ -2,6 +2,7 @@ package borderhacks2020.gamestate;
 
 import borderhacks2020.EventBasedState;
 import borderhacks2020.Main;
+import borderhacks2020.MapManager;
 import borderhacks2020.ui.Button;
 import borderhacks2020.ui.ImageComponent;
 import borderhacks2020.ui.ShapeComponent;
@@ -13,7 +14,10 @@ import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.util.FontUtils;
+import sun.font.FontUtilities;
 
+import javax.swing.plaf.FontUIResource;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -22,13 +26,15 @@ import java.util.Locale;
 public class PlayState extends EventBasedState {
 
     private Image mapImg, graphImg;
-    private ImageComponent selector, bigMap;
+    private ImageComponent selector, bigMap, playbutton;
     private boolean mapSelected = true;
     private ShapeComponent[] bars = new ShapeComponent[3];
     private Calendar calendar;
     private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.CANADA);
-    private Label lblDate;
+    private Label lblDate, lblTotal, lblActive, lblDeaths, lblRecovered;
     private int timer;
+    private boolean gameTicking = true;
+    private MapManager manager;
 
     private int activeCases;
     private float infectionRate;
@@ -53,7 +59,7 @@ public class PlayState extends EventBasedState {
             public void onLeftClick(){
                 mapSelected = true;
                 selector.setImage(mapImg);
-                bigMap.setEnabled(true);
+                bigMap.setLocation(465, 619);
             }
         });
         components.add(new Button(gameContainer, 1186, 941, 165, 128){
@@ -61,7 +67,17 @@ public class PlayState extends EventBasedState {
             public void onLeftClick(){
                 mapSelected = false;
                 selector.setImage(graphImg);
-                bigMap.setEnabled(false);
+                bigMap.setLocation(10000, 10000);
+            }
+        });
+        bigMap = new ImageComponent(gameContainer, "assets/map.png", 465, 619, 780, 728);
+        components.add(bigMap);
+        playbutton = new ImageComponent(gameContainer, "assets/gamestate/playbtn.png", 10000, 10000, 90, 83);
+        components.add(playbutton);
+        components.add(new Button(gameContainer, 705, 78, 90, 83){
+            @Override
+            public void onLeftClick(){
+                gameTicking = !gameTicking;
             }
         });
         bars[0] = new ShapeComponent(gameContainer, 1192, 468, 0, 53, new Color(0x47bc4f), Color.transparent);
@@ -70,16 +86,33 @@ public class PlayState extends EventBasedState {
         for (ShapeComponent s: bars) {
             components.add(s);
         }
-        updateBar(gameContainer, 1, 0.34f);
         activeCases = 100;
         happiness = 1;
         economy = 1;
-        infectionRate = 2;
+        infectionRate = .25f;
         happinessRate = 0.99f;
         economyRate = 1.01f;
-        recoveryRate = 0.5f;
+        recoveryRate = 0.18f;
         deaths = 0;
         totalCases = 100;
+        calendar = Calendar.getInstance();
+        calendar.set(2020, 0,1);
+        lblDate = new Label(gameContainer, dateFormat.format(calendar.getTime()), 451, 60, Main.pixelFontBlack);
+        components.add(lblDate);
+
+        lblTotal = new Label(gameContainer, Integer.toString(totalCases), 1342, 125, Main.pixelFontBlack, FontUtils.Alignment.RIGHT);
+        components.add(lblTotal);
+        lblActive = new Label(gameContainer, Integer.toString(activeCases), 1860, 125, Main.pixelFontBlack, FontUtils.Alignment.RIGHT);
+        components.add(lblActive);
+        lblDeaths = new Label(gameContainer, Integer.toString(deaths), 1342, 290, Main.pixelFontBlack, FontUtils.Alignment.RIGHT);
+        components.add(lblDeaths);
+        lblRecovered = new Label(gameContainer, Integer.toString(totalCases-activeCases-deaths), 1860, 290, Main.pixelFontBlack, FontUtils.Alignment.RIGHT);
+        components.add(lblRecovered);
+
+        updateBar(gameContainer, 0, happiness);
+        updateBar(gameContainer, 1, economy);
+        updateBar(gameContainer, 2, infectionRate - recoveryRate);
+        manager = new MapManager();
     }
 
     public void updateBar(GameContainer gameContainer, int barNum, float progress) throws SlickException {
@@ -89,30 +122,40 @@ public class PlayState extends EventBasedState {
 
     public void render(GameContainer gameContainer, StateBasedGame game, Graphics g) throws SlickException {
         super.render(gameContainer, game, g);
-        lblDate = new Label(gameContainer, "date", 451, 60, Main.pixelFontBlack);
-        components.add(lblDate);
-        calendar = Calendar.getInstance();
-        calendar.set(2020, 0,1);
-        lblDate.setText(dateFormat.format(calendar.getTime()));
-
-        bigMap = new ImageComponent(gameContainer, "assets/map.png", 465, 619, 780, 728);
-        components.add(bigMap);
-
-
+        if(mapSelected){
+            manager.render(gameContainer, g);
+        }
     }
 
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
-        timer += delta;
-        if(timer > 1000){
-            timer -= 1000;
-            calendar.add(Calendar.DAY_OF_MONTH, 1);
-            lblDate.setText(dateFormat.format(calendar.getTime()));
-            deaths += activeCases *0.05;
-            activeCases = Math.round(activeCases *(infectionRate-recoveryRate));
-            totalCases += activeCases * infectionRate;
-            happiness *= happinessRate;
-            economy *= economyRate;
+        if(gameTicking){
+            timer += delta;
+            if(timer > 1000){
+                timer -= 1000;
+                calendar.add(Calendar.DAY_OF_MONTH, 1);
+                lblDate.setText(dateFormat.format(calendar.getTime()));
+                deaths += activeCases *0.05;
+                int newCases = Math.round(activeCases *(infectionRate-recoveryRate));
+                activeCases += newCases;
+                manager.add(container, newCases/10);
+                totalCases += activeCases * infectionRate;
+                happiness *= happinessRate;
+                economy *= economyRate;
+
+                lblTotal.setText(Integer.toString(totalCases));
+                lblActive.setText(Integer.toString(activeCases));
+                lblDeaths.setText(Integer.toString(deaths));
+                lblRecovered.setText(Integer.toString(totalCases-activeCases-deaths));
+
+                updateBar(container, 0, happiness);
+                updateBar(container, 1, economy);
+                updateBar(container, 2, infectionRate - recoveryRate);
+            }
+            playbutton.setLocation(10000, 10000);
+        }
+        else {
+            playbutton.setLocation(705, 78);
         }
     }
 
