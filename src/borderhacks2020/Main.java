a532547dc5a109b75c1f7441495307ba35533e52
Tony Fang <fangtony11@gmail.com>
2020-09-25 23:39:53 -0600

    Main Menu

--- src/borderhacks2020/Main.java
+++ src/borderhacks2020/Main.java
@@ -1,5 +1,6 @@
 package borderhacks2020;
 
+import borderhacks2020.mainmenu.MainMenu;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
@@ -7,7 +8,7 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class Main extends StateBasedGame {
 
-    public static final int WIDTH = 1000, HEIGHT = 600;
+    public static final int WIDTH = 1920, HEIGHT = 1080;
 
     public Main() {
         super("Pandemic!!!");
@@ -15,13 +16,13 @@ public class Main extends StateBasedGame {
 
     @Override
     public void initStatesList(GameContainer gameContainer) throws SlickException {
-
+        addState(new MainMenu());
     }
 
     public static void main(String args[]){
         try {
             AppGameContainer app = new AppGameContainer(new Main()); //create game in container
-            app.setDisplayMode(WIDTH, HEIGHT, false); //set window size and if fullscreen
+            app.setDisplayMode(WIDTH, HEIGHT, true); //set window size and if fullscreen
             app.setVSync(true);
             app.start();
         } catch (SlickException e) {
--- src/borderhacks2020/mainmenu/MainMenu.java
+++ src/borderhacks2020/mainmenu/MainMenu.java
@@ -1,9 +1,11 @@
 package borderhacks2020.mainmenu;
 
 import borderhacks2020.EventBasedState;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import borderhacks2020.Main;
+import borderhacks2020.ui.Button;
+import borderhacks2020.ui.ImageComponent;
+import borderhacks2020.ui.Label;
+import org.newdawn.slick.*;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -11,7 +13,15 @@ public class MainMenu extends EventBasedState {
 
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-
+        components.add(new ImageComponent(gameContainer, "assets/title_screen/background.png", Main.WIDTH/2, Main.HEIGHT/2, Main.WIDTH, Main.HEIGHT));
+        components.add(new Label(gameContainer, "Sample Text", Main.WIDTH/2, Main.HEIGHT/4));
+        Button playBtn = new Button(gameContainer, Main.WIDTH/2, Main.HEIGHT/2, 200, 100, new Image("assets/title_screen/playbtn.png")) {
+            @Override
+            public void onLeftClick() {
+                //stateBasedGame.enterState(1);
+            }
+        };
+        components.add(playBtn);
     }
 
     @Override
