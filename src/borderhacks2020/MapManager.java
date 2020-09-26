ac45bbe1136c25574130f8f30de53b2a7e38a6c4
Tony Fang <fangtony11@gmail.com>
2020-09-26 17:15:08 -0600

    Zone density for cases

--- src/borderhacks2020/MapManager.java
+++ src/borderhacks2020/MapManager.java
@@ -9,14 +9,46 @@ import org.newdawn.slick.SlickException;
 import java.util.ArrayList;
 
 public class MapManager {
-    public static final int lfBound = 0, rtBound = 400, upBound = 0, lwBound = 400;
     private ArrayList<ShapeComponent> cases = new ArrayList<ShapeComponent>();
 
     public void add (GameContainer gameContainer, int numOfCases) {
-        int width = rtBound - lfBound;
-        int height = lwBound - upBound;
+        int zone = 0;
+        float weight = 0;
+        int x = 0;
+        int y = 0;
         for (int i = 0; i < numOfCases; i++) {
-            cases.add(new ShapeComponent(gameContainer, (int) (Math.random() * width) + lfBound, (int) (Math.random() * height) + upBound, 4, 4, Color.red, Color.transparent));
+            weight = (float) Math.random();
+            if (weight > 0.45)
+                zone = 0;
+            else if (weight > 0.30)
+                zone = 1;
+            else if (weight > 0.05)
+                zone = 2;
+            else if (weight > 0.025)
+                zone = 3;
+            else
+                zone = 4;
+            if (zone == 0) {    // residential zone
+                x = (int) (Math.random() * (615 - 390)) + 390;
+                y = (int) (Math.random() * (848 - 413)) + 413;
+            }
+            else if (zone == 1) {   // airport zone
+                x = (int) (Math.random() * (390 - 150)) + 150;
+                y = (int) (Math.random() * (615 - 533)) + 533;
+            }
+            else if (zone == 2) {   // university zone
+                x = (int) (Math.random() * (773 - 637)) + 637;
+                y = (int) (Math.random() * (428 - 322)) + 322;
+            }
+            else if (zone == 3) {   // boat 1
+                x = (int) (Math.random() * (195 - 165)) + 165;
+                y = (int) (Math.random() * (758 - 727)) + 727;
+            }
+            else if (zone == 4) {   // boat 2
+                x = (int) (Math.random() * (818 - 788)) + 788;
+                y = (int) (Math.random() * (600 - 570)) + 570;
+            }
+            cases.add(new ShapeComponent(gameContainer, x, y, 4, 4, Color.red, Color.transparent));
         }
     }
 
