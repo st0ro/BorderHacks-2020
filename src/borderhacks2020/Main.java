2516a7d99b751ac97ce237ac37e20903a7b619b8
st0ro <46041969+st0ro@users.noreply.github.com>
2020-09-25 19:16:54 -0600

    Initial project commit

---
+++ src/borderhacks2020/Main.java
@@ -0,0 +1,16 @@
+package borderhacks2020;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Main extends StateBasedGame {
+    public Main(String name) {
+        super(name);
+    }
+
+    @Override
+    public void initStatesList(GameContainer gameContainer) throws SlickException {
+
+    }
+}
