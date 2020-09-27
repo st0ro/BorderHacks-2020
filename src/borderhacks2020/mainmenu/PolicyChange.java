38175f16626b389eb82a2f7d8c2c54680a9c2fd8
jameswzhou <56765078+jameswzhou@users.noreply.github.com>
2020-09-26 20:44:24 -0600

    More game balancing

--- src/borderhacks2020/mainmenu/PolicyChange.java
+++ src/borderhacks2020/mainmenu/PolicyChange.java
@@ -31,23 +31,26 @@ public class PolicyChange extends EventBasedState {
         otherPolicyBox = new Slot(gameContainer, 4 * Main.WIDTH / 5, Main.HEIGHT / 4, 1 * Main.WIDTH / 10, Main.HEIGHT / 4, Color.decode("0x69D15B"), Color.lightGray);
         components.add(otherPolicyBox);
         Policy[] policies = {
-                new Policy(gameContainer, 250, 700, 150, 150, new Image("assets/policies/regular.png"), socialPolicyBox, 0, 0, 0.001f),
-                new Policy(gameContainer, 450, 700, 150, 150, new Image("assets/policies/mandatorymasks.png"), socialPolicyBox, -0.01f, -0.0005f, -0.001f),
-                new Policy(gameContainer, 650, 700, 150, 150, new Image("assets/policies/nogatherings.png"), socialPolicyBox, -0.015f, -0.001f, -0.0015f),
-                new Policy(gameContainer, 850, 700, 150, 150, new Image("assets/policies/lockdown.png"), socialPolicyBox,-0.05f, -0.01f, -0.01f),
-                new Policy(gameContainer, 1050, 700, 150, 150, new Image("assets/policies/openborders.png"), internationalPolicyBox, 0.03f, 0.005f, 0.001f),
-                new Policy(gameContainer, 1250, 700, 150, 150, new Image("assets/policies/restrictedtravel.png"), internationalPolicyBox, 0.01f, 0.001f, 0),
-                new Policy(gameContainer, 1450, 700, 150, 150, new Image("assets/policies/minimalcontact.png"), internationalPolicyBox, 0f, 0f, -0.001f),
-                new Policy(gameContainer, 1650, 700, 150, 150, new Image("assets/policies/isolation.png"), internationalPolicyBox, -0.02f, -0.001f, -0.003f),
-                new Policy(gameContainer, 250, 900, 150, 150, new Image("assets/policies/sociallydistance.png"), economicPolicyBox, -0.005f, 0f, -0.0005f),
-                new Policy(gameContainer, 450, 900, 150, 150, new Image("assets/policies/normaloperation.png"), economicPolicyBox, 0.005f, 0.001f, 0f),
-                new Policy(gameContainer, 650, 900, 150, 150, new Image("assets/policies/essentialsonly.png"), economicPolicyBox, -0.01f, -0.001f, -0.001f),
-                new Policy(gameContainer, 850, 900, 150, 150, new Image("assets/policies/completeclosure.png"), economicPolicyBox, -0.05f, -0.01f, -0.01f),
-                new Policy(gameContainer, 1050, 900, 150, 150, new Image("assets/policies/stimuluscheck.png"), otherPolicyBox, -0.02f, -0.001f, 0f),
-                new Policy(gameContainer, 1250, 900, 150, 150, new Image("assets/policies/construction.png"), otherPolicyBox, 0f, 0.002f, 0f),
-                new Policy(gameContainer, 1450, 900, 150, 150, new Image("assets/policies/acceleratedresearch.png"), otherPolicyBox, 0f, -0.001f, 0f),
-                new Policy(gameContainer, 1650, 900, 150, 150, new Image("assets/policies/deceleratedresearch.png"), otherPolicyBox, 0f, 0.001f, 0f),
+                new Policy(gameContainer, 250, 700, 150, 150, new Image("assets/policies/regular.png"), socialPolicyBox, 0.03f, 0.3f, 0.2f),
+                new Policy(gameContainer, 450, 700, 150, 150, new Image("assets/policies/mandatorymasks.png"), socialPolicyBox, 0f, -0.1f, -0.1f),
+                new Policy(gameContainer, 650, 700, 150, 150, new Image("assets/policies/nogatherings.png"), socialPolicyBox, -0.01f, -0.2f, -0.3f),
+                new Policy(gameContainer, 850, 700, 150, 150, new Image("assets/policies/lockdown.png"), socialPolicyBox,-0.02f, -0.4f, -0.5f),
+                new Policy(gameContainer, 1050, 700, 150, 150, new Image("assets/policies/openborders.png"), internationalPolicyBox, 0.02f, 0.1f, 0.1f),
+                new Policy(gameContainer, 1250, 700, 150, 150, new Image("assets/policies/restrictedtravel.png"), internationalPolicyBox, 0f, 0f, 0),
+                new Policy(gameContainer, 1450, 700, 150, 150, new Image("assets/policies/minimalcontact.png"), internationalPolicyBox, -0.01f, -0.1f, -0.1f),
+                new Policy(gameContainer, 1650, 700, 150, 150, new Image("assets/policies/isolation.png"), internationalPolicyBox, -0.02f, -0.3f, -0.3f),
+                new Policy(gameContainer, 250, 900, 150, 150, new Image("assets/policies/normaloperation.png"), economicPolicyBox, 0.03f, 0.1f, 0.2f),
+                new Policy(gameContainer, 450, 900, 150, 150, new Image("assets/policies/sociallydistance.png"), economicPolicyBox, 0f, 0f, -0.1f),
+                new Policy(gameContainer, 650, 900, 150, 150, new Image("assets/policies/essentialsonly.png"), economicPolicyBox, -0.01f, -0.2f, -0.3f),
+                new Policy(gameContainer, 850, 900, 150, 150, new Image("assets/policies/completeclosure.png"), economicPolicyBox, -0.03f, -0.6f, -0.4f),
+                new Policy(gameContainer, 1050, 900, 150, 150, new Image("assets/policies/stimuluscheck.png"), otherPolicyBox, 0f, -0.3f, 0.2f),
+                new Policy(gameContainer, 1250, 900, 150, 150, new Image("assets/policies/construction.png"), otherPolicyBox, 0.01f, 0.3f, 0.01f),
+                new Policy(gameContainer, 1450, 900, 150, 150, new Image("assets/policies/acceleratedresearch.png"), otherPolicyBox, 0f, -0.2f, 0f),
+                new Policy(gameContainer, 1650, 900, 150, 150, new Image("assets/policies/deceleratedresearch.png"), otherPolicyBox, 0f, 0.2f, 0f),
         };
+        policies[0].switchLocation();
+        policies[4].switchLocation();
+        policies[8].switchLocation();
         components.add(new Button(gameContainer, Main.WIDTH/2, Main.HEIGHT/2 - 50, 150, 75, new Image("assets/title_screen/playbtn.png")){
             @Override
             public void onLeftClick(){
@@ -93,8 +96,8 @@ public class PolicyChange extends EventBasedState {
             hm+=otherPolicyBox.getImplemented().getHappinessModifier();
             im+=otherPolicyBox.getImplemented().getInfectionModifier();
         }
-        Main.playState.setEconomyModifier(em);
-        Main.playState.setHappinessModifier(hm);
+        Main.playState.setEconomyRate(em);
+        Main.playState.setHappinessRate(hm);
         Main.playState.setInfectionModifier(im);
 
         stateBasedGame.enterState(2);
